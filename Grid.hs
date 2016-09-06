b3e05c8967f4e5c5b2594441e51b47ce00595db8
hanjoes <hanzhou87@gmail.com>
2016-09-06 16:49:19 -0700

    tweak

--- Grid.hs
+++ Grid.hs
@@ -6,27 +6,31 @@ import Graphics.UI.GLUT
 -- Note: the row and column are all starting from 0.
 data Grid = Grid { r :: GLfloat, c :: GLfloat, s :: GLfloat }
 
+-- Padding scale.
+ps :: GLfloat
+ps = 8.0
+
 left :: Grid -> GLfloat
-left g = (c g) * (s g) + (s g) / 5.0
+left g = -1 + 2 * ((c g) * (s g) + (s g) / ps)
 
 right :: Grid -> GLfloat
-right g = (c g + 1) * (s g) - (s g) / 5.0
+right g = -1 + 2 * ((c g + 1) * (s g) - (s g) / ps)
 
 top :: Grid -> GLfloat
-top g = (r g) * (s g) + (s g) / 5.0
+top g = 1 - 2 * ((r g) * (s g) + (s g) / ps)
 
 bottom :: Grid -> GLfloat
-bottom g = (r g + 1) * (s g) - (s g) / 5.0
+bottom g = 1 - 2 * ((r g + 1) * (s g) - (s g) / ps)
 
 -- GLUT uses some kind of unit coordinate system. Weird!
 renderGrid :: Grid -> IO ()
 renderGrid g = do
   renderPrimitive Quads $ do
   color $ Color3 0 0 (1 :: GLfloat)
-  vertex $ Vertex2 (-1 + 2 * (left g)) (1 - 2 * (top g))
-  vertex $ Vertex2 (-1 + 2 * (right g)) (1 - 2 * (top g))
-  vertex $ Vertex2 (-1 + 2 * (right g)) (1 - 2 * (bottom g))
-  vertex $ Vertex2 (-1 + 2 * (left g)) (1 - 2 * (bottom g))
+  vertex $ Vertex2 (left g) (top g)
+  vertex $ Vertex2 (right g) (top g)
+  vertex $ Vertex2 (right g) (bottom g)
+  vertex $ Vertex2 (left g) (bottom g)
 
 renderGrids :: GLfloat -> GLfloat -> IO ()
 renderGrids ws s
